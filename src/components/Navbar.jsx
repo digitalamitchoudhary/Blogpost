b44b7bfdb71ae2ab6e3d0b8c153220e048224b27
Digitalamitchoudhary <amitchoudhary.dmp@gmail.com>
2025-12-24 23:28:13 +0530

    navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,29 @@
-import { Link } from "react-router-dom";
+import {useNavigate } from "react-router-dom";
+import { assets } from '../assets/assets';
 
 const Navbar = () => {
+  const navigate = useNavigate(); 
   return (
     <nav className="w-full bg-white fixed top-0 left-0 z-50">
       <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
 
         {/* Logo */}
         <div className="flex items-center gap-2">
-          <div className="w-9 h-9 rounded-lg bg-blue-600 flex items-center justify-center text-white font-bold">
-            Q
-          </div>
-          <span className="text-xl font-semibold text-gray-800">
-            Quickblog
-          </span>
+          <img 
+            onClick={() => navigate('/')} 
+            src={assets.logo} 
+            alt="logo" 
+            className='w-32 sm:w-44 cursor-pointer' 
+          />
         </div>
 
         {/* Right Button */}
-        <Link
-          to="/dashboard"
-          className="bg-blue-600 text-white px-5 py-2 rounded-full font-medium hover:bg-blue-700 transition"
-        >
-          Dashboard →
-        </Link>
+        <button 
+          onClick={() => navigate('/admin')} 
+          className='flex items-center gap-2 rounded-full text-sm cursor-pointer bg-primary text-white px-10 py-2.5'
+        > 
+          Admin  <img src={assets.arrow} className='w-3' alt="arrow " />
+        </button>
 
       </div>
     </nav>
